5ca69909d96d21e4dfcfec18bd7de9586c2301a9
EverettButtars <EverettButtars@gmail.com>
2025-03-15 21:22:00 -0600

    Noise Scoring, Time Limit, and Game Over

--- GamemodeController.gd
+++ GamemodeController.gd
@@ -12,7 +12,7 @@ func _ready():
 	teamNoiseScore = 0
 	trashScore = 0
 	players[0] = $Player
-	players[0].translate(Vector3(0, 2, 0))
+	players[0].translate(Vector3(0, 4, 0))
 	for i in range(1,numPlayers):
 		players.append(load("res://player.tscn").instantiate())
 		print("ADDED NEW PLAYER!!!!")
@@ -25,13 +25,19 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
+func _process(delta):
 	$HUD.update_trash_score(str(trashScore))
 	$HUD.update_team_noise_score(float(teamNoiseScore))
-	$HUD.update_time_remaining(str(timeRemaining))
+	$HUD.update_time_remaining(str(int(timeRemaining)))
 	$HUD.update_noise_score(players[0].noise_score)
 	#print(len(players))
 	update_team_noise()
+	if(teamNoiseScore >= 100):
+		$HUD/GameOverScreen.text = "Game Over"
+	if(timeRemaining <= 0):
+		$HUD/GameOverScreen.text = "Game Over"
+		
+	timeRemaining -= delta	
 	
 func update_team_noise():
 	teamNoiseScore = 0
